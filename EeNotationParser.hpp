ee703a18c0a479fc30d8c794fd23e5d0b591b65d
Sainan <sainan@calamity.inc>
2024-12-22 06:27:13 +0100

    Avoid 'using namespace' in the header file

--- EeNotationParser.hpp
+++ EeNotationParser.hpp
@@ -6,19 +6,17 @@
 #include <string.hpp>
 #include <UniquePtr.hpp>
 
-using namespace soup;
-
 class EeNotationParser
 {
 protected:
-	std::stack<JsonNode*> stack{};
+	std::stack<soup::JsonNode*> stack{};
 	std::string key;
 	std::string buf;
 
 public:
-	[[nodiscard]] UniquePtr<JsonObject> parse(const std::string& data)
+	[[nodiscard]] soup::UniquePtr<soup::JsonObject> parse(const std::string& data)
 	{
-		auto root = soup::make_unique<JsonObject>();
+		auto root = soup::make_unique<soup::JsonObject>();
 		stack.push(root.get());
 		bool pending_obj_or_arr = false;
 		bool is_string = false;
@@ -42,7 +40,7 @@ public:
 				if (pending_obj_or_arr)
 				{
 					pending_obj_or_arr = false;
-					pushAndAscend(soup::make_unique<JsonObject>());
+					pushAndAscend(soup::make_unique<soup::JsonObject>());
 					key = std::move(buf);
 				}
 				else
@@ -50,14 +48,14 @@ public:
 					key = std::move(buf);
 				}
 				buf.clear();
-				string::trim(key);
+				soup::string::trim(key);
 			}
 			else if (c == '{')
 			{
 				buf.clear(); // discard indentation
 				if (pending_obj_or_arr)
 				{
-					pushAndAscend(soup::make_unique<JsonArray>());
+					pushAndAscend(soup::make_unique<soup::JsonArray>());
 				}
 				pending_obj_or_arr = true;
 			}
@@ -66,12 +64,12 @@ public:
 				if (pending_obj_or_arr)
 				{
 					pending_obj_or_arr = false;
-					pushAndAscend(soup::make_unique<JsonArray>());
+					pushAndAscend(soup::make_unique<soup::JsonArray>());
 					dischargeBuffer();
 				}
 				else
 				{
-					string::trim(buf);
+					soup::string::trim(buf);
 					if (!buf.empty())
 					{
 						dischargeBuffer();
@@ -83,9 +81,9 @@ public:
 				if (pending_obj_or_arr)
 				{
 					pending_obj_or_arr = false;
-					pushAndAscend(soup::make_unique<JsonArray>());
+					pushAndAscend(soup::make_unique<soup::JsonArray>());
 				}
-				string::trim(buf);
+				soup::string::trim(buf);
 				if (!buf.empty())
 				{
 					dischargeBuffer();
@@ -97,7 +95,7 @@ public:
 				if (pending_obj_or_arr)
 				{
 					pending_obj_or_arr = false;
-					pushAndAscend(soup::make_unique<JsonArray>());
+					pushAndAscend(soup::make_unique<soup::JsonArray>());
 				}
 				if (buf.empty())
 				{
@@ -112,7 +110,7 @@ public:
 			{
 				if (!pending_obj_or_arr)
 				{
-					string::trim(buf);
+					soup::string::trim(buf);
 					if (!buf.empty())
 					{
 						dischargeBuffer();
@@ -141,11 +139,11 @@ protected:
 		{
 			if (buf.empty())
 			{
-				pushValue(soup::make_unique<JsonString>(std::move(buf)));
+				pushValue(soup::make_unique<soup::JsonString>(std::move(buf)));
 			}
 			else
 			{
-				pushValue(soup::make_unique<JsonInt>(ival));
+				pushValue(soup::make_unique<soup::JsonInt>(ival));
 			}
 		}
 		else if (endptr != buf.data())
@@ -153,37 +151,37 @@ protected:
 			auto fval = strtod(buf.data(), &endptr);
 			if (endptr == buf.data() + buf.size())
 			{
-				pushValue(soup::make_unique<JsonFloat>(fval));
+				pushValue(soup::make_unique<soup::JsonFloat>(fval));
 			}
 			else
 			{
-				pushValue(soup::make_unique<JsonString>(std::move(buf)));
+				pushValue(soup::make_unique<soup::JsonString>(std::move(buf)));
 			}
 		}
 		else
 		{
-			pushValue(soup::make_unique<JsonString>(std::move(buf)));
+			pushValue(soup::make_unique<soup::JsonString>(std::move(buf)));
 		}
 		buf.clear();
 	}
 
-	void pushAndAscend(UniquePtr<JsonNode> value)
+	void pushAndAscend(soup::UniquePtr<soup::JsonNode> value)
 	{
-		SOUP_ASSERT(value->type == JSON_ARRAY || value->type == JSON_OBJECT);
+		SOUP_ASSERT(value->type == soup::JSON_ARRAY || value->type == soup::JSON_OBJECT);
 		auto ptr = value.get();
 		pushValue(std::move(value));
 		stack.push(ptr);
 	}
 
-	void pushValue(UniquePtr<JsonNode> value)
+	void pushValue(soup::UniquePtr<soup::JsonNode> value)
 	{
-		if (stack.top()->type == JSON_ARRAY)
+		if (stack.top()->type == soup::JSON_ARRAY)
 		{
-			static_cast<JsonArray*>(stack.top())->children.emplace_back(std::move(value));
+			static_cast<soup::JsonArray*>(stack.top())->children.emplace_back(std::move(value));
 		}
 		else
 		{
-			static_cast<JsonObject*>(stack.top())->add(std::move(key), std::move(value));
+			static_cast<soup::JsonObject*>(stack.top())->add(std::move(key), std::move(value));
 			key.clear();
 		}
 	}
